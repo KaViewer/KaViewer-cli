32ecd0f487efe20d86ac582d58f0e2ecf405bb78
KoyZhuang <koy@ko8e24.top>
2022-09-02 16:01:41 +0800

    init.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kaviewer/kaviewer-cli
+
+go 1.16
+
+require github.com/spf13/cobra v1.5.0
---
+++ runner.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kaviewer/kaviewer-cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
